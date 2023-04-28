92907148e7d802078b13df288bd24e9f80543c7a
voidrulz1 <131723217+voidrulz1@users.noreply.github.com>
2023-04-28 12:59:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,66 +1,48 @@
+"use strict";
+
+require('dotenv').config();
+
+const Monitor = require('ping-monitor')
+const express = require('express');
+const server = express();
+
+const Hangbot = require('./hangbot.js')
+//const Musicbot = require('./musicbot.js')
+
+server.all('/', (req, res) => {
+  res.send('<h2>Server is ready!</h2>');
+});
+
+server.get('/records.js', function(req, res) {
+  res.sendFile(__dirname + "/records.js");
+});
+
+server.listen(process.env.EXPRESS_PORT, () => {
+    console.log('Server Ready.');
+  });
 
 
-const Monitor = require('ping-monitor');
+const myMonitor = new Monitor({
+  address: process.env.PING_TARGET,
+  title: process.env.PING_TARGET,
+  interval: 1
+});
 
-
-const keepAlive = require('./server');
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.type('html').send(html));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
-
+myMonitor.on('up', function (res, state) {
+    console.log('Yay!! ' + state.address + ' is up.');
+});
+myMonitor.on('down', function (res, state) {
+  console.log('Oh Snap!! ' + state.address + ' is down! ' + state.statusMessage);
+});
+myMonitor.on('restored', function (res, state) {
+  console.log(state.address + ' has been restore');
+});
+myMonitor.on('stop', function (res, state) {
+  console.log(state.address + ' monitor has stopped.');
+});
+myMonitor.on('timeout', function (error, res) {
+  console.log(error);
+});
+myMonitor.on('error', function (error) {
+  console.log(error);
+});
