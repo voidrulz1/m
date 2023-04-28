3ab94ec09c31f4ce0cdba12b0049bd15ca103016
voidrulz1 <131723217+voidrulz1@users.noreply.github.com>
2023-04-28 17:07:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const express = require('express');
 const server = express();
 
 //const Hangbot = require('./hangbot.js')
-//const Musicbot = require('./musicbot.js')
-const Bot = require('./bot.js')
+const Musicbot = require('./musicbot.js')
+//const Bot = require('./bot.js')
 
 server.all('/', (req, res) => {
   res.send('<h2>Server is ready!</h2>');
